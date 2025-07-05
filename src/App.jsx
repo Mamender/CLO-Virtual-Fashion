cb66816ff74517fed200321a600bec5042484335
Mamender <mamendershaw1418@gmail.com>
2025-07-05 22:25:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+function App() {
+ 
+
+  return (
+    <>
+      <div className="container mt-5">
+      <h1>Hello React + Vite + Bootstrap</h1>
+      <button className="btn btn-primary">Test Button</button>
+    </div>
+    </>
+  )
+}
+
+export default App
